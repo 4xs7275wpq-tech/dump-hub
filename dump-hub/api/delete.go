4fcfda0d552b7d07e63208287089bbba637fdbc1
4xs7275wpq-tech <4xs7275wpq@privaterelay.appleid.com>
2026-02-21 09:47:04 -0600

    Update delete.go

--- dump-hub/api/delete.go
+++ dump-hub/api/delete.go
@@ -1,4 +1,37 @@
-package api
+>>> from bs4 import BeautifulSoup
+>>> soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+>>> print(soup.prettify())
+<html>
+ <body>
+  <p>
+   Some
+   <b>
+    bad
+    <i>
+     HTML
+    </i>
+   </b>
+  </p>
+ </body>
+</html>
+>>> soup.find(string="bad")
+'bad'
+>>> soup.i
+<i>HTML</i>
+#
+>>> soup = BeautifulSoup("<tag1>Some<tag2/>bad<tag3>XML", "xml")
+#
+>>> print(soup.prettify())
+<?xml version="1.0" encoding="utf-8"?>
+<tag1>
+ Some
+ <tag2/>
+ bad
+ <tag3>
+  XML
+ </tag3>
+</tag1>
+```
 
 /*
 The MIT License (MIT)
